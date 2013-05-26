664f210aaff0113560fcea505a6128f7c1a55d2e
Mark Hallett <git@markhallett.co.uk>
2013-05-27 00:09:47 +0100

    Basic structure

---
+++ timegit.py
@@ -0,0 +1,38 @@
+# timegit
+
+class TimeGit(object):
+    def __init__(self,config_file_name):
+        # read config info        
+        pass
+
+
+    def run(self):
+        print 'run'
+        
+        # get repo info
+        # for each repo version
+        #    check out repo
+        #    run test code in repo
+        #
+        # diaplay results
+        #
+                
+        
+        
+        
+        print 'Done'
+        
+        pass
+    
+
+# --------------------
+
+def test():
+    config_file_name = 'abc.txt'
+    
+    myTimeGit = TimeGit(config_file_name)
+    
+    myTimeGit.run()   
+    
+    
+test()

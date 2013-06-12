19880a8f608bc682d5c8e8235e837339c913dc43
Mark Hallett <git@markhallett.co.uk>
2013-06-12 22:34:24 +0100

    start of refactoring

--- timegit.py
+++ timegit.py
@@ -37,26 +37,22 @@ class TimeGit(object):
              print '  test_module', self.test_module
              print '  test_function', self.test_function
 
-
-    def run(self):
-        '''
-        Short script to extract, run, time, and display the performance
-        of code in github
-        '''
-        # get repo info
-        
+    def _prep(self):
+        pass
         # (make) cd to data dir           
         if not os.path.exists(self.data_dir):
-            os.mkdir(self.data_dir)
+           os.mkdir(self.data_dir)
         os.chdir(self.data_dir)
-    
-        
+   
+       
         repo_path,fileExtension = os.path.splitext(self.git_repo)   
         self.repo_name = repo_path.split('/')[-1] 
         print self.repo_name 
-        
+       
         data_dir = os.path.join(self.data_dir,self.repo_name +'_data')
         
+        
+       
         if os.path.exists(data_dir):
             os.chdir(data_dir) 
             os.chdir(self.repo_name)
@@ -64,62 +60,65 @@ class TimeGit(object):
             # data dir does not exist, create it, clone the repo, and get the revisions
             # only needed first time around, delete data dir to refresh
             os.mkdir(data_dir)                                   
-         
+        
             print data_dir  
             os.chdir(data_dir)           
-        
+       
+       
             cmd = r"git clone %s" %self.git_repo
             print cmd
             try:
                 os.system(cmd)
             except:
                 pass
-        
+       
             os.chdir(self.repo_name)
             cmd = r"git log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short > ../commits.txt"
             print cmd
-            os.system(cmd)
-        
-    
-        # read the file containing the git revisions
-        # TODO change to with
+            os.system(cmd)    
+
+
+    def _getdatafromgithub(self):
+        pass
+
+    def _getgitcommitids(self):
         f = open('../commits.txt')
-        
         commit_ids = []
         for count, line in enumerate(f):
-            rev = line.split(' ')[1] 
-            #print rev
-            commit_ids.append(rev)
-        f.close()
-        
+                  rev = line.split(' ')[1] 
+                  #print rev
+                  commit_ids.append(rev)
+        f.close()      
         commit_ids.reverse()
-        
+        return commit_ids
+              
+    def _runtestfunction(self, commit_ids):
         # add data dir to sys.path ??        
         sys.path.append('.')
-        
+          
         times = [0.0,] # start from origin
         print '-----------'
-        # for each repo version
+          # for each repo version
         for count, commit_id in enumerate(commit_ids):        
             #if count == 3: break
             print '...........'
             print count,commit_id
-    
+      
             cmd =  'git checkout %s' %commit_id
             #print cmd
             os.system(cmd) #TODO check return code
-
+  
             # clean up new revision, del pyc and reload modules
             # use walk for os independance
             os.system('find . -name \*.pyc |xargs rm') 
-            
+              
             for mod in sys.modules.values():
                 if mod:
                     try:
                         reload(mod) 
                     except:
                         pass
-                       
+                         
             # TODO confing the funtion to run
             try:
                 cmd = "import %s" %self.test_module
@@ -128,7 +127,7 @@ class TimeGit(object):
                 print "No timing module (%s) in this revision. %s" %(self.test_module,str(e))
                 times.append(0)
                 continue
-            
+              
             start = time.time()
             try:
                 cmd = "%s.%s" %(self.test_module, self.test_function)
@@ -138,23 +137,38 @@ class TimeGit(object):
                 times.append(0)
                 continue
             run_time = time.time() - start
- 
+   
             print 'run_time', run_time
-            times.append(run_time) 
-            
-        print 'times',times
-        x = range(len(times))
-        y = times      
+            times.append(run_time)         
+        return times
+    
+    
+    def _show(self,times):
         # display results
-
         # unfortunate but must import here
         import matplotlib.pyplot as plt
-        
+        print 'times',times       
+        x = range(len(times))
+        y = times         
+
         fig = plt.figure()
         plt.title(self.git_repo)
-       
+            
         plt.plot(x, y)        
-        plt.show()
+        plt.show()       
+        
+    
+    def run(self):
+        '''
+        Short script to extract, run, time, and display the performance
+        of code in github
+        '''
+        
+        self._prep()
+        self._getdatafromgithub()
+        commit_ids = self._getgitcommitids()
+        times = self._runtestfunction(commit_ids)  
+        self._show(times)
             
         print 'Done'
             
